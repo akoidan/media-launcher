00aee973b1412c84dfe762e34d94bc0a372dda34
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2026-08-08 19:27:07 +0200

    das

--- Formula/media-launcher.rb
+++ Formula/media-launcher.rb
@@ -1,13 +1,22 @@
 class MediaLauncher < Formula
   desc "Matches audio/subtitle files to episodes and launches them in mpv or VLC"
   homepage "https://github.com/akoidan/media-launcher"
-  url "https://github.com/akoidan/media-launcher.git", tag: "v0.1.2"
   version "0.1.2"
 
-  depends_on "rust" => :build
+  on_arm do
+    url "https://github.com/akoidan/media-launcher/releases/download/v0.1.2/media-launcher-macos-arm64"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+
+  on_intel do
+    url "https://github.com/akoidan/media-launcher/releases/download/v0.1.2/media-launcher-macos-x86_64"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    binary = Dir["media-launcher*"].reject { |f| f.end_with?(".rb") }.first
+    bin.install binary => "media-launcher"
+    chmod "+x", bin/"media-launcher"
   end
 
   test do
